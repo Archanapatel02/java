aafe887f8159a4617067bc7e2bb944315cbc02a3
Archana patel <archanapatel0824@gmail.com>
2026-04-17 14:37:15 +0530

    frist commit

---
+++ hello.java
@@ -0,0 +1,58 @@
+// 1.wap to print hello world
+class hello{
+    public static void main(String[] args){
+        System.out.print("hello world");
+    }
+}
+// 2.wap to print add two number
+
+//         int a = 9;
+//         int b = 8;
+//         System.out.print(a + b);
+//     }
+// }
+//3.type of conversion and casting
+
+// byte a = 21;
+// int b = 563;
+// a= (byte)b;  
+// System.out.print(a);
+
+// float a= 5.1f;
+// int b = 72;
+// a = (float)b;
+// System.out.print(a);
+
+// 4 . wap to use arithematic operates 
+// int a = 9;
+// int b = 8;
+// int s = a + b ;
+// int f = a * b;
+// int d = a % b ;
+// int t = a / b ;
+// System.out.println(s);
+// System.out.println(f);
+// System.out.println(d);
+// System.out.println(t);
+
+// 5 . wap to use logical operates
+// int a = 9;
+// int b = 8;
+// boolean c=a>b && b>a ;
+// boolean d= a<b || b<a;
+// System.out.println(c);
+// System.out.println(d);
+// System.out.println(!d);
+
+//  6. wap to calaculate a percentage 
+// float chemistry = 89;
+// float maths = 90;
+// float physic = 85;
+// float english = 99;
+// float computer = 88;
+// float result = (chemistry + maths + physic + english + computer)/5;
+// System.out.println(result);
+
+
+}
+    }
