73d48a15bb2ab6ea43cadb6f503d511108c3da34
Archana patel <archanapatel0824@gmail.com>
2026-04-17 15:24:58 +0530

    second

--- ja1.java
+++ ja1.java
@@ -1,58 +1,24 @@
-// 1.wap to print hello world
-class ja1{
-    public static void main(String[] args){
-//         System.out.print("hello world");
-//     }
-// }
-// 2.wap to print add two number
-
-//         int a = 9;
-//         int b = 8;
-//         System.out.print(a + b);
-//     }
-// }
-//3.type of conversion and casting
-
-// byte a = 21;
-// int b = 563;
-// a= (byte)b;  
-// System.out.print(a);
-
-// float a= 5.1f;
-// int b = 72;
-// a = (float)b;
-// System.out.print(a);
-
-// 4 . wap to use arithematic operates 
-// int a = 9;
-// int b = 8;
-// int s = a + b ;
-// int f = a * b;
-// int d = a % b ;
-// int t = a / b ;
-// System.out.println(s);
-// System.out.println(f);
-// System.out.println(d);
-// System.out.println(t);
-
-// 5 . wap to use logical operates
-// int a = 9;
-// int b = 8;
-// boolean c=a>b && b>a ;
-// boolean d= a<b || b<a;
-// System.out.println(c);
-// System.out.println(d);
-// System.out.println(!d);
-
-//  6. wap to calaculate a percentage 
-float chemistry = 89;
-float maths = 90;
-float physic = 85;
-float english = 99;
-float computer = 88;
-float result = (chemistry + maths + physic + english + computer)/5;
-System.out.println(result);
-
-
-}
+// 1.take input bu user and add two number
+import java.util.Scanner;
+public class ja1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // System.out.println("enter a frist number");
+        // int a = sc.nextInt();
+        // System.out.println("enter a second number");
+        // int b = sc.nextInt();
+        // int sum = a+b;
+        // System.out.println("sum = "+ sum);
+//2. wap to take principle ,rate and time as input from the user ans calculate simple inetrest
+System.out.println("enter a principle");
+float p = sc.nextFloat();
+System.out.println("enter a rate");
+float r = sc.nextFloat();
+System.out.println("enter a time");
+float t =sc.nextFloat();
+float SI = (p*r*t)/100;
+System.out.println("simple interest =" + SI);
+        
     }
+    
+}
